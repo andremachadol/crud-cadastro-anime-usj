914e1a8d955eb7c95fdc657c6b2f52b3376f785f
andremachadol <andre-luizmachado@hotmail.com>
2021-05-27 18:38:18 -0300

    Incluido Função Deletar

--- src/main/java/br/edu/usj/ads/pw/cadastro/AnimeController.java
+++ src/main/java/br/edu/usj/ads/pw/cadastro/AnimeController.java
@@ -1,11 +1,16 @@
 package br.edu.usj.ads.pw.cadastro;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 
 
@@ -15,7 +20,35 @@ public class AnimeController {
     
     @Autowired
     AnimeRepository animeRepository;
+
+
+    @GetMapping(value="/")
+    public ModelAndView getIndex() {
+        List<Anime> lista = new ArrayList<>();
+       
+        lista = animeRepository.findAll();
+
+        ModelAndView modelAndView = new ModelAndView("index");
+
+        modelAndView.addObject("lista", lista);
+        
+        return modelAndView;
+    }
+
+    @GetMapping(value="/detalhes/{id}")
+    public ModelAndView getDetalhes(@PathVariable Long id) {
+        Anime anime= new Anime ();
+        
+        anime=animeRepository.findById(id).get();
+       
+        ModelAndView modelAndView = new ModelAndView("detalhes");
+
+        modelAndView.addObject("anime", anime);
+
+        return modelAndView;
+    }
     
+
     @GetMapping(value="/cadastro")
     public ModelAndView getCadastro() {
         
@@ -31,8 +64,20 @@ public class AnimeController {
         anime.setEps(eps);
         
         animeRepository.save(anime);
+        ModelAndView modelAndView = new ModelAndView("detalhes");
+
+        modelAndView.addObject("anime", anime);
+
+        return modelAndView;
+    }
+    @GetMapping(value="/deletar/{id}")
+    public String getDeletar(@PathVariable Long id) {
         
-        return null;
+        animeRepository.deleteById(id);
+
+        return "redirect:/";
+       
+        
     }
     
 }
